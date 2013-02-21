b186923deb3deba52c0b0a9610005e475f0b9912
Peerames <pom.peerames@gmail.com>
2013-02-21 15:41:20 +0700

    Photo view on tableview

---
+++ MemoCamera/MCAlbumViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCAlbumViewController.h
+//  MemoCamera
+//
+//  Created by Peerames on 2/21/13.
+//  Copyright (c) 2013 Peerames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCAlbumViewController : UIViewController
+
+@end
---
+++ MemoCamera/MCAlbumViewController.m
@@ -0,0 +1,38 @@
+//
+//  MCAlbumViewController.m
+//  MemoCamera
+//
+//  Created by Peerames on 2/21/13.
+//  Copyright (c) 2013 Peerames. All rights reserved.
+//
+
+#import "MCAlbumViewController.h"
+
+@interface MCAlbumViewController ()
+
+@end
+
+@implementation MCAlbumViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
